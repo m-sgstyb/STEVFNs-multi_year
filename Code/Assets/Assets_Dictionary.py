47db9aa33191f137a6481a3aca8933dfbd55c19b
Mónica Sagastuy Breña <monica.sagastuybrena@eng.ox.ac.uk>
2024-12-04 11:21:02 +0000

    Added BESS_Existing asset to asset dictionary

--- Code/Assets/Assets_Dictionary.py
+++ Code/Assets/Assets_Dictionary.py
@@ -11,6 +11,7 @@ from .HTH_Demand import HTH_Demand_Asset
 from .CG import CG_Asset
 from .EL_to_HTH import EL_to_HTH_Asset
 from .BESS import BESS_Asset
+from .BESS_Existing import BESS_Existing_Asset
 from .EL_to_NH3 import EL_to_NH3_Asset
 from .NH3_to_EL import NH3_to_EL_Asset
 from .NH3_Storage import NH3_Storage_Asset
@@ -40,6 +41,7 @@ ASSET_DICT = {EL_Demand_Asset.asset_name: EL_Demand_Asset,
               CG_Asset.asset_name: CG_Asset,
               EL_to_HTH_Asset.asset_name: EL_to_HTH_Asset,
               BESS_Asset.asset_name: BESS_Asset,
+              BESS_Existing_Asset.asset_name: BESS_Existing_Asset,
               EL_to_NH3_Asset.asset_name: EL_to_NH3_Asset,
               NH3_to_EL_Asset.asset_name: NH3_to_EL_Asset,
               NH3_Storage_Asset.asset_name: NH3_Storage_Asset,
